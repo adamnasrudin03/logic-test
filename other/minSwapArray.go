06b91d4da00a800bab72a8ffb1fb3779d257e2b5
Adam Nasrudin <adamnasrudin858@gmail.com>
2023-03-14 14:01:50 +0700

    adjust min swap array using asc loop

--- other/minSwapArray.go
+++ other/minSwapArray.go
@@ -1,40 +1,51 @@
 package other
 
-import "fmt"
-
 func MinSwapArray(arr []int) int {
 	// Initialise count variable
 	count := 0
-	i := 0
-	fmt.Printf("arr %v, index saat ini %v \n", arr, i)
-	for i < len(arr) {
-		fmt.Printf("CEK PERTAMA 1 array: %v, array i: %v, i+1 = %v, result if: %v \n", arr, arr[i], i+1, arr[i] != i+1)
-		// If current element is not at the right position
-		// Jika elemen saat ini tidak pada posisi yang tepat
-		if arr[i] != i+1 {
+	// solution by using asc loop
+	temp := 0
+	min := 0
+	for i := 0; i < len(arr); i++ {
+		min = i
+		for j := i + 1; j < len(arr); j++ {
+			if arr[j] < arr[min] {
 
-			fmt.Printf("FOR KEDUA 2 array: %v, array i: %v, i+1 = %v, result if: %v \n", arr, arr[i], i+1, arr[i] != i+1)
-			for arr[i] != i+1 {
-				temp := 0
-				fmt.Printf(" arr: %v  , index: %v \n", arr, i)
-
-				// Swap current element with correct position of that element
-				// Tukar elemen saat ini dengan posisi yang benar dari elemen itu
-				temp = arr[arr[i]-1]
-
-				arr[arr[i]-1] = arr[i]
-				arr[i] = temp
-				fmt.Printf(" arr: %v  , index: %v \n", arr, i)
-				fmt.Println()
+				// changing the index to show the min value
+				min = j
 				count++
+				break
 			}
-			fmt.Printf("FOR KEDUA 2 SELESAI array: %v, array i: %v, i+1 = %v, result if: %v \n", arr, arr[i], i+1, arr[i] != i+1)
 		}
-
-		// Increment for next index when current element is at correct position
-		// Inkremen untuk indeks berikutnya saat elemen saat ini berada di posisi yang benar
-		i++
+		temp = arr[i]
+		arr[i] = arr[min]
+		arr[min] = temp
 	}
-	fmt.Printf("FINAL arr %v, index saat ini %v \n", arr, i)
+
+	// another solution
+	// i := 0
+	// for i < len(arr) {
+	// 	// If current element is not at the right position
+	// 	// Jika elemen saat ini tidak pada posisi yang tepat
+	// 	if arr[i] != i+1 {
+
+	// 		for arr[i] != i+1 {
+	// 			temp := 0
+	// 			fmt.Printf(" arr: %v  , index: %v \n", arr, i)
+
+	// 			// Swap current element with correct position of that element
+	// 			// Tukar elemen saat ini dengan posisi yang benar dari elemen itu
+	// 			temp = arr[arr[i]-1]
+
+	// 			arr[arr[i]-1] = arr[i]
+	// 			arr[i] = temp
+	// 			count++
+	// 		}
+	// 	}
+
+	// 	// Increment for next index when current element is at correct position
+	// 	// Inkremen untuk indeks berikutnya saat elemen saat ini berada di posisi yang benar
+	// 	i++
+	// }
 	return count
 }
